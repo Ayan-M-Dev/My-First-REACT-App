3d154e457fde3b54220467f4e35dd822cffaafda
Ayan Masood <ayan.m.dev@gmail.com>
2023-01-22 10:42:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import <file name>
+import {<components>} from <location of components>
+ 
+function <Function Name>{
+   <More JavaScript Code>
+ 
+   return(
+      <JSX>
+   )
 }
-
-export default App;
+ 
+export default <Function Name></Function>
