1a4d38813a34cea526ef445182959621d8109cbb
Ayan Masood <ayan.m.dev@gmail.com>
2023-01-22 10:44:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import <file name>
-import {<components>} from <location of components>
+import './App.css';
+import { useState } from 'react';
  
-function <Function Name>{
-   <More JavaScript Code>
+function App() {
+ return (
+  <div className="App">
+   <div className="innerContainer">
+    <h1>Counter App</h1>
+    <p style={{textAlign:"center"}}>This is a counter application built by stackieusername, powered by React</p>
+    <input type="input" value={counter} onChange={(e)=>{setCounter(e.target.value)}}/>
+    <div className="buttonContainer">
+     <button onClick={()=>{
+      if(counter > minimum){
+       setCounter(counter - counterIncrements)
+      }
+     }}>-</button>
+     <button onClick={()=>{
+      if(counter < maxium){
+       setCounter(counter + counterIncrements)
+      }
+     }}>+</button>
+    </div>
  
-   return(
-      <JSX>
-   )
+    <h2 style={{marginTop:"50px"}}>other settings</h2>
+    <p>By increments</p>
+    <input type="input" value={counterIncrements} onChange={(e)=>{setCounterIncrements(e.target.value)}}/>
+    <div className="buttonContainer">
+     <button onClick={()=>{setCounterIncrements(counterIncrements-1)}}>-</button>
+     <button onClick={()=>{setCounterIncrements(counterIncrements+1)}}>+</button>
+    </div>
+ 
+    <p>Maximum</p>
+    <input type="input" value={maxium} onChange={(e)=>{setMaxium(e.target.value)}}/>
+    <div className="buttonContainer">
+     <button onClick={()=>{setMaxium(maxium-1)}}>-</button>
+     <button onClick={()=>{setMaxium(maxium+1)}}>+</button>
+    </div>
+ 
+    <p>Minimum</p>
+    <input type="input" value={minimum} onChange={(e)=>{setMinimum(e.target.value)}}/>
+    <div className="buttonContainer">
+     <button onClick={()=>{setMinimum(minimum-1)}}>-</button>
+     <button onClick={()=>{setMinimum(minimum+1)}}>+</button>
+    </div>
+   </div>
+  </div>
+ );
 }
  
-export default <Function Name></Function>
+export default App;
